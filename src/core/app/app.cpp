7c1012bf85e9d712b1dc38f4cfaf8e0a62bbbd66
Filip Lobpreis <f.lobpreis@gmail.com>
2025-06-07 16:46:49 +0200

    wip: app: implement camera
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- src/core/app/app.cpp
+++ src/core/app/app.cpp
@@ -2,7 +2,9 @@
 
 #include <algorithm>
 #include <execution>
+#include <raylib.h>
 #include <raymath.h>
+#include <rcamera.h>
 #include <print>
 
 namespace rl
@@ -32,6 +34,7 @@ Application::Application(const Config& config)
 		m_camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };		  // Camera up vector (rotation towards target)
 		m_camera.fovy = 30.0f;								// Camera field-of-view Y
 		m_camera.projection = CAMERA_PERSPECTIVE;			 // Camera type
+
 	}
 	else {
 		m_camera = *m_config.camera;
@@ -54,7 +57,8 @@ void Application::run()
 	SetWindowState(FLAG_VSYNC_HINT);
 	SetWindowOpacity(m_config.opacity);
 	SetWindowSize(m_config.screenWidth, m_config.screenHeight);
-	auto [x, y] = m_config.windowPosition;
+
+	UpdateCamera(&m_camera, CAMERA_THIRD_PERSON);
 
 	for (auto object : m_objects) {
 		object->loadModel();
@@ -67,12 +71,32 @@ void Application::run()
 			return;
 		}
 
+		if (IsKeyDown(KEY_ONE)) {
+			m_camera.up = Vector3{ 1.0f, 0.0f, 0.0f };
+		}
+		else if (IsKeyDown(KEY_TWO)) {
+			m_camera.up = Vector3{ 0.0f, 1.0f, 0.0f };
+		}
+		else if (IsKeyDown(KEY_THREE)) {
+			m_camera.up = Vector3{ 0.0f, 10.0f, 0.0f };
+		}
+
 		BeginDrawing();
 			ClearBackground(RAYWHITE);
 
+			m_camera.target = m_objects[0]->rlModel().position;
+			auto cameraPos = m_objects[0]->rotation().toRotationMatrix() * Vector3f{ 0.0f, 50.0f, -120.0f };
+
+			m_camera.position = Vector3Add(m_camera.target, Vector3{ cameraPos.x(), cameraPos.y(), cameraPos.z() });
+			// m_camera.up = Vector3{ 0.0f, 1.0f, 0.0f };
+
+			// CameraRoll(&m_camera, m_objects[0]->rotation().toEuler().y());
+			// CameraPitch(&m_camera, m_objects[0]->rotation().toEuler().x(), true, true, false);
+			UpdateCamera(&m_camera, CAMERA_THIRD_PERSON);
+
 			BeginMode3D(m_camera);
 
-			DrawGrid(10, 10.0f);
+			DrawGrid(100, 10.0f);
 
 			double fps = GetFPS();
 			fps = (fps == 0) ? 1 : fps;
