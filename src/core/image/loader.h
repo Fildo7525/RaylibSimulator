e272fe5ecaadfa5664fe29a5fad59f19f66966de
Filip Lobpreis <f.lobpreis@gmail.com>
2025-06-22 09:59:34 +0200

    loader: remove unused constructor
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- src/core/image/loader.h
+++ src/core/image/loader.h
@@ -29,30 +29,6 @@ struct Model
 	};
 
 	Model() = default;
-	Model(const std::string &modelPath,
-		  const std::string &texturePath,
-		  float thrust,
-		  float dThrust,
-		  float dMoment,
-		  float moment,
-		  const Vector3 &position = Vector3{ 0.0f, 0.0f, 0.0f },
-		  const Vector3 &rotation = Vector3{ 0.0f, 0.0f, 0.0f },
-		  float scale = 1.0f,
-		  float mass = 1.0f,
-		  const Camera &camera = Camera{ Vector3{ 0.0f, 0.0f, 0.0f }, Vector3{ 0.0f, 1.0f, 0.0f }, 30.0f })
-		: modelPath(modelPath)
-		, texturePath(texturePath)
-		, position(position)
-		, rotation(rotation)
-		, scale(scale)
-		, mass(mass)
-		, camera(camera)
-		, thrust(thrust)
-		, moment(moment)
-		, dThrust(dThrust)
-		, dMoment(dMoment)
-	{
-	}
 
 	/**
 	 * @brief Loads a model configuration from a file.
