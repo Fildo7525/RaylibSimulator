f863a91407ca8b82a9b11352d5387779c22f1ac3
Filip Lobpreis <f.lobpreis@gmail.com>
2025-07-27 20:30:55 +0200

    core: load gravity unit vector
    Each model may have different orientation. Thus, the gravity orientation
    can be changed this way.
    
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- src/core/image/loader.cpp
+++ src/core/image/loader.cpp
@@ -34,6 +34,9 @@ rl::Model rl::Model::fromFile(const rl::Path &configPath)
 	read = jsonConfig.value("rotation", std::array<float, 3>{ 0.0f, 0.0f, 0.0f });
 	config.rotation = Vector3{ read[0], read[1], read[2] };
 
+	read = jsonConfig.value("gravity", std::array<float, 3>{ 0.0f, 0.0f, 1.0f });
+	config.gravity = Vector3{ read[0], read[1], read[2] };
+
 	// Camera configuration
 	read = jsonConfig.value("camera", json::object()).value("offset", std::array<float, 3>{ 0.0f, 0.0f, 0.0f });
 	config.camera.offset = Vector3{ read[0], read[1], read[2] };
