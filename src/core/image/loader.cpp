5e0b970241c8645c11fc210f451bf5a044b103c9
Filip Lobpreis <f.lobpreis@gmail.com>
2025-08-02 16:03:30 +0200

    core: loader: display all the loaded data
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- src/core/image/loader.cpp
+++ src/core/image/loader.cpp
@@ -71,16 +71,40 @@ rl::Model rl::Model::fromFile(const rl::Path &configPath)
 		};
 	}
 
-	std::println("Loaded configuration: modelPath={}\n texturePath={}\n position=({:.2f}, {:.2f}, {:.2f})\n "
-		"rotation=({:.2f}, {:.2f}, {:.2f})\n scale={:.2f}\n mass={:.2f}\n camera.position=({:.2f}, {:.2f}, {:.2f})\n "
-		"camera.rotation=({:.2f}, {:.2f}, {:.2f})\n camera.fovY={:.2f}\n thrust={:.2f}\n moment={:.2f}",
-		config.modelPath, config.texturePath,
+	std::println("Loaded configuration: \n"
+		"Model Path: {}\n"
+		"Texture Path: {}\n"
+		"Scale: {}\n"
+		"Mass: {}\n"
+		"Position: [{}, {}, {}]\n"
+		"Rotation: [{}, {}, {}]\n"
+		"Thurst: [{}, {}]\n"
+		"dThrust: {}\n"
+		"Moment: [{}, {}]\n"
+		"dMoment: {}\n"
+		"Gravity: [{}, {}, {}]\n"
+		"Camera Offset: [{}, {}, {}]\n"
+		"Camera Up: [{}, {}, {}]\n"
+		"Camera FOV: {}\n"
+		"Thrust Range: [{}, {}]\n"
+		"Moment Range: [{}, {}]\n",
+		config.modelPath,
+		config.texturePath,
+		config.scale,
+		config.mass,
 		config.position.x, config.position.y, config.position.z,
 		config.rotation.x, config.rotation.y, config.rotation.z,
-		config.scale, config.mass,
+		config.thrust.x, config.thrust.y,
+		config.dThrust,
+		config.moment.x, config.moment.y,
+		config.dMoment,
+		config.gravity.x, config.gravity.y, config.gravity.z,
 		config.camera.offset.x, config.camera.offset.y, config.camera.offset.z,
 		config.camera.up.x, config.camera.up.y, config.camera.up.z,
-		config.camera.fovY, config.dThrust, config.dMoment);
+		config.camera.fovY,
+		config.thrust.x, config.thrust.y,
+		config.moment.x, config.moment.y
+	);
 	return config;
 }
 
